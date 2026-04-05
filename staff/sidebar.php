03af8878ce366c03d5ae8cbbf0f5fb857ec34139
kamcfriedrice <kamilu_luis@dlsu.edu.ph>
2026-04-05 15:28:30 +0800

    Included sidebar

---
+++ staff/sidebar.php
@@ -0,0 +1,17 @@
+<div class="sidebar">
+    <div class="logo">
+        <h1>SmartClinic</h1>
+        <span>Staff Panel</span>
+    </div>
+
+    <div class="menu">
+        <a href="dashboard.php" class="nav-item">Dashboard</a>
+        <a href="manage_schedule.php" class="nav-item">Manage Schedule</a>
+        <a href="manage_appointments.php" class="nav-item">Appointments</a>
+        <a href="manage_queue.php" class="nav-item">Queue</a>
+    </div>
+
+    <div class="logout">
+        <a href="../logout.php" class="nav-item">Logout</a>
+    </div>
+</div>
